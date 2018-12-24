3db8ff87b762e061a5a6ff06d91f7c3e6fdc28b4
MitchellMint <github@mitchellmint.com>
2018-12-23 18:56:44 -0600

    Update amount.h

--- src/amount.h
+++ src/amount.h
@@ -27,7 +27,7 @@ extern const std::string CURRENCY_UNIT;
  * critical; in unusual circumstances like a(nother) overflow bug that allowed
  * for the creation of coins out of thin air modification could lead to a fork.
  * */
-static const CAmount MAX_MONEY = 550000000 * COIN;
+static const CAmount MAX_MONEY = 55000000 * COIN; //55.000.000
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 /** Type-safe wrapper class for fee rates
