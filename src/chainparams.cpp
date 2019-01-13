9812bf681cd7fad6355fc4e63ec7db98583bca2d
MitchellMint <github@mitchellmint.com>
2019-01-13 11:08:46 -0600

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -86,8 +86,8 @@ public:
         consensus.BIP34Height = 227931; // FIX
         consensus.BIP34Hash = uint256S("0x000000000000024b89b42a942fe0d9fea3bb44ab7bd1b19115dd6a759c0808b8"); // FIX
         consensus.powLimit = uint256S("00000fffff000000000000000000000000000000000000000000000000000000");
-        consensus.nPowTargetTimespan = 2 * 60; // TRUSTplus: every blocks
-        consensus.nPowTargetSpacing = 1 * 60; // TRUSTplus: 60 seconds
+        consensus.nPowTargetTimespan = 2 * 45; // TRUSTplus: every blocks
+        consensus.nPowTargetSpacing = 1 * 45; // TRUSTplus: 45 seconds
         consensus.fPowAllowMinDifficultyBlocks = false;
         consensus.fPowNoRetargeting = false;
         consensus.nRuleChangeActivationThreshold = 1916; // 95% of 2016
